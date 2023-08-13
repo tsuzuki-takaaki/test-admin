756baf68eabe113b6c8e6db9d2bd80a67ca09245
alexanderpierce <takaaki.110706@gmail.com>
2023-08-13 15:52:26 +0900

    Update UserList cypress component test

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { Admin, EditGuesser, ListGuesser, Resource } from "react-admin";
+import { Admin, Resource } from "react-admin";
 import { authProvider } from "./authProvider";
 import { dataProvider } from "./dataProvider";
 import { UserList } from "./UserList";
 import { PostList } from "./PostList";
 import { PostEdit } from "./PostEdit";
 import { PostCreate } from "./PostCreate";
+import { UserEdit } from "./UserEdit";
+import { UserCreate } from "./UserCreate";
 
 export const App = () => {
   return (
     <Admin authProvider={authProvider} dataProvider={dataProvider}>
       <Resource
         name="users"
+        create={UserCreate}
+        edit={UserEdit}
         list={UserList}
         recordRepresentation="name"
       />
--- src/UserList.cy.tsx
+++ src/UserList.cy.tsx
@@ -1,10 +1,12 @@
-import { AdminContext, GetListResult, Resource, testDataProvider } from 'react-admin'
+import { Admin, GetListResult, Resource, testDataProvider } from 'react-admin'
 import { UserList } from './UserList'
+import { UserCreate } from './UserCreate'
+import { UserEdit } from './UserEdit'
 
 describe('<UserList />', () => {
   it('hello world', () => {
     cy.mount(
-      <AdminContext
+      <Admin
         dataProvider={testDataProvider({
           getList: () => 
             Promise.resolve<GetListResult>({
@@ -18,8 +20,8 @@ describe('<UserList />', () => {
             })
         })}
       >
-        <Resource name="users" list={UserList} />
-      </AdminContext>
+        <Resource name="users" list={UserList} create={UserCreate} edit={UserEdit} />
+      </Admin>
     )
     cy.get('td.column-id > span').should('have.text', '1')
     cy.get('td.column-name > span').should('have.text', 'hello world')
