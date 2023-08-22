9f5ce65219d232d73bd1f80a3f483bedfb87b57c
alexanderpierce <takaaki.110706@gmail.com>
2023-08-22 22:46:21 +0900

    Update UserList

--- src/UserList.tsx
+++ src/UserList.tsx
@@ -1,22 +1,12 @@
-// in src/users.tsx
-import { List, SimpleList, Datagrid, TextField } from "react-admin";
+import { List, Datagrid, TextField } from "react-admin";
 
 export const UserList = () => {
-  const isSmall = null //useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
   return (
     <List>
-      {isSmall ? (
-        <SimpleList
-          primaryText={(record) => record.name}
-          secondaryText={(record) => record.username}
-          tertiaryText={(record) => record.email}
-        />
-      ) : (
-        <Datagrid rowClick="edit">
-          <TextField source="id" />
-          <TextField source="name" />
-        </Datagrid>
-      )}
+      <Datagrid rowClick="edit">
+        <TextField source="id" />
+        <TextField source="name" />
+      </Datagrid>
     </List>
   );
 };
